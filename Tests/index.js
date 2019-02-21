33b05233431317878982668e3ffd9b39a75b85ff
Chris Talman <cewe.wow@gmail.com>
2019-02-21 20:24:38 +0000

    Introduced test file.

---
+++ Tests/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const { promise: request } = require('../index.js');
+
+request
+(
+    {
+        method: 'GET',
+        path: 'https://localhost:3060',
+        tls:
+        {
+            cert: './cert.pem',
+            key: './key.pem'
+        }
+    }
+);
