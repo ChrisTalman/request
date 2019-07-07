93131c9fc6aa611a6bce3c8a7f6f37456e22a83b
Chris Talman <cewe.mail@gmail.com>
2019-07-07 21:53:48 +0100

    Fixed indentation.

--- index.d.ts
+++ index.d.ts
@@ -4,93 +4,93 @@ import { Response } from '@bluecewe/isomorphic-fetch';
 // Module
 declare module '@bluecewe/request'
 {
-    export type PromiseCallback = <GenericJsonSuccess> (definition: Definition) => Promise<Result <GenericJsonSuccess>>;
+	export type PromiseCallback = <GenericJsonSuccess> (definition: Definition) => Promise<Result <GenericJsonSuccess>>;
 	export const promise: PromiseCallback;
-    // Domain
-    export class Domain
-    {
-        public path: string;
-        public type: Definition['type'];
-        public auth: ConstructorParameters['auth'];
+	// Domain
+	export class Domain
+	{
+		public path: string;
+		public type: Definition['type'];
+		public auth: ConstructorParameters['auth'];
 		/**
-	        If provided, guarantees a query string parameter containing the body as stringified JSON, for GET and HEAD requests. The given string is used as the parameter name.
-	        If unprovided, the request body will be the standard request body, and will throw an exception for GET and HEAD requests.
-	    */
+			If provided, guarantees a query string parameter containing the body as stringified JSON, for GET and HEAD requests. The given string is used as the parameter name.
+			If unprovided, the request body will be the standard request body, and will throw an exception for GET and HEAD requests.
+		*/
 		public queryBody: ConstructorParameters['queryBody'];
-        public tls: ConstructorParameters['tls'];
-        constructor(parameters: ConstructorParameters);
-        public promise: PromiseCallback;
-    }
-    export interface ConstructorParameters
-    {
-        path?: string;
-        type?: Definition['type'];
-        auth?: Auth;
-    	/** @see Domain.queryBody */
-        queryBody?: string;
-        tls?: Definition['tls'];
-    }
-    export type Auth = string | AuthCallback;
-    /**
-        string: token value for Authorization header
-        false: Authorization header will not be sent
-    */
-    export type AuthCallback = () => string | false;
+		public tls: ConstructorParameters['tls'];
+		constructor(parameters: ConstructorParameters);
+		public promise: PromiseCallback;
+	}
+	export interface ConstructorParameters
+	{
+		path?: string;
+		type?: Definition['type'];
+		auth?: Auth;
+		/** @see Domain.queryBody */
+		queryBody?: string;
+		tls?: Definition['tls'];
+	}
+	export type Auth = string | AuthCallback;
+	/**
+		string: token value for Authorization header
+		false: Authorization header will not be sent
+	*/
+	export type AuthCallback = () => string | false;
 	// Request Error
-    export type RequestErrorType =
-        'rawResponse'
-        | 'jsonResponse'
-        | 'unexpected'
-    ;
+	export type RequestErrorType =
+		'rawResponse'
+		| 'jsonResponse'
+		| 'unexpected'
+	;
 	export class RequestError extends Error
 	{
 		public readonly type: RequestErrorType;
 	}
 	export class RequestJsonError <GenericJsonError extends object> extends RequestError
 	{
-	    public readonly type: 'jsonResponse';
-	    public readonly json: GenericJsonError;
-	    public readonly response: Response;
-    }
-    export class RequestRawError extends RequestError
-    {
-        public readonly type: 'rawResponse';
-        public readonly response: Response;
-    }
+		public readonly type: 'jsonResponse';
+		public readonly json: GenericJsonError;
+		public readonly response: Response;
+	}
+	export class RequestRawError extends RequestError
+	{
+		public readonly type: 'rawResponse';
+		public readonly response: Response;
+	}
 }
 
 // Definition
 export interface Definition
 {
-    path: string;
-    method: string;
+	path: string;
+	method: string;
 	type?: 'application/json' | 'application/x-www-form-urlencoded';
-    auth?: string;
+	auth?: string;
 	headers?: DefinitionHeaders;
-    body?: DefinitionBody;
-    jsonResponseError?: boolean;
-    jsonResponseSuccess?: boolean;
-    tls?: DefinitionTls;
+	body?: DefinitionBody;
+	jsonResponseError?: boolean;
+	jsonResponseSuccess?: boolean;
+	tls?: DefinitionTls;
 }
 export interface DefinitionHeaders
 {
-    [key: string]: any;
+	[key: string]: any;
 }
 export interface DefinitionBody
 {
-    [key: string]: any;
+	[key: string]: any;
 }
 export interface DefinitionTls
 {
-    /** Path to public key file. */
-    cert: string;
-    /** Path to private key file. */
-    key: string;
+	/** Path to public key file. */
+	cert: string;
+	/** Path to private key file. */
+	key: string;
 }
 
 // Result
 export class Result <GenericJsonSuccess>
 {
-    public response: Response;
-    public json?: GenericJsonSuccess;
+	public response: Response;
+	public json?: GenericJsonSuccess;
 }
