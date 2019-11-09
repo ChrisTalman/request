37dcfb1b1f843444f5792086803cbb84deb5f731
Chris Talman <cewe.mail@gmail.com>
2019-11-09 17:23:48 +0000

    Updated type 'RequestFunction'; ignored /index.js.

--- index.d.ts
+++ index.d.ts
@@ -5,7 +5,7 @@ import { Response } from '@ChrisTalman/isomorphic-fetch';
 declare module '@chris-talman/request'
 {
 	// Request
-	export type RequestFunction = <GenericJsonSuccess> (definition: Definition) => Promise<Result <GenericJsonSuccess>>;
+	export type RequestFunction = <GenericJsonSuccess, GenericResult extends Result<GenericJsonSuccess>> (definition: Definition) => Promise<GenericResult>;
 	export const request: RequestFunction;
 	// Domain
 	export class Domain
